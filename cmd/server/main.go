a4f279c0791c6bdf55a9cc9ecf6e05a9301fac90
Leandro Leite <leandroleite.ll25@proton.me>
2025-08-26 17:18:42 -0300

    feat: add crud methods to products

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/LeandroASLeite/goexpert/APIS/configs"
-	"github.com/LeandroASLeite/goexpert/APIS/internal/dto"
 	"github.com/LeandroASLeite/goexpert/APIS/internal/entity"
 	"github.com/LeandroASLeite/goexpert/APIS/internal/infra/database"
+	"github.com/LeandroASLeite/goexpert/APIS/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -24,39 +25,12 @@ func main() {
 	}
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
 	productDB := database.NewProduct(db)
-	productHandler := newProductHandler(productDB)
-
-	http.HandleFunc("/products", productHandler.CreateProduct)
-
-	http.ListenAndServe(":8000", nil)
-}
-
-type ProductHandler struct {
-	ProductDB database.ProductInterface
-}
-
-func newProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductInputDTO
-	err := json.NewDecoder(r.Body).Decode(&product)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	p, err := entity.NewProduct(product.Name, product.Price)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	err = h.ProductDB.Create(p)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
+	productHandler := handlers.NewProductHandler(productDB)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	http.ListenAndServe(":8000", r)
 }
